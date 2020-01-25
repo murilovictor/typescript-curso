f36c961be9965910927085e6e181ee0d4ef4089d
Murilo Victor <murilovictor63@gmail.com>
2020-01-25 16:17:31 -0300

    Exemplos utilizando variaveis tipadas, configuração de compilação no tsconfig

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # typescript-curso
+
+### Comandos
+npm install typescript -g -- instala o typescript
+tsc -v -- versão
+tsc app.ts -- compila o arquivo
+tsc -w -- fica monitorando a pasta
+node dist/app.js -- executa o arquivo
---
+++ app.ts
@@ -0,0 +1,14 @@
+let mensagem: string = "Murilo"
+console.log(mensagem)
+
+let episode: number = 4
+
+console.log(episode) 
+episode = episode + 1
+console.log('Next episode is: ' + episode)
+
+
+let favoriteDroid: string
+
+favoriteDroid = "BB-8"
+console.log(favoriteDroid)
